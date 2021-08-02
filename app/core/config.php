a488e0c3cd2c374c8feb8ec0488bbed835dd557a
tnebes <tomislav.nebes@gmail.com>
2021-08-02 09:55:11 +0200

    Added config file

---
+++ app/core/config.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+   define('APP_NAME', 'IWTL Bootstrap');
+   define('APP_AUTHOR', 'tnebes');
+
+   define('URL_ROOT', 'bootstrap.com');
+   define('APP_ROOT', dirname(dirname(__FILE__)));
+
+   define('DB_HOST', 'localhost');
+   define('DB_NAME', 'iwtl');
+   define('DB_USER', 'edunova');
+   define('DB_PASS', 'edunova');
+
+   /* Constants for the database */
+
+?>
