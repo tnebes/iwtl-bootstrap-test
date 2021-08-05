187e3fafca85a0fb4813ad8ae2207765f691192c
tnebes <tomislav.nebes@gmail.com>
2021-08-05 11:31:13 +0200

    Deployment version

--- app/views/er/index.php
+++ app/views/er/index.php
@@ -11,31 +11,35 @@
       require_once(APP_ROOT . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'navigator.php');
    ?>
 
-   <!-- add diagram img here -->
-   <div class="d-flex flex-column">
-      <div class="p-2">
-         <img src="<?php new Image('er' . DIRECTORY_SEPARATOR . 'iwtl_diagram.png')?>" alt="ER Diagram" class="img-fluid mx-auto d-block" style="max-width: 100%;">
+   <main class="py-4">
+      <div class="alert alert-light w-50 mx-auto">
+         <h1 class="h1 fw-normal text-center">ER Diagram and SQL Code</h1>
       </div>
-      <div class="p-2 mx-auto my-5 bg-light">
-         <h1 class="h1 text-dark px-5 mt-4">SQL code</h1>
-         <?php
-            $file = fopen(APP_ROOT . '/content/er/i_want_to_learn.sql', 'r');
-            // read and echo file line by line
-            echo '<pre class="my-5 mx-5">';
-            $counter = 0;
-            while(!feof($file)) 
-            {
-               $line = fgets($file);
-               echo '<code>';
-               echo sprintf('%3d| ', $counter++);
-               echo $line;
-               echo '</code>';
-               echo '<br/>';
-            }
-            echo '</pre>';
-         ?>
+      <div class="d-flex flex-column">
+         <div class="p-2">
+            <img src="<?php new Image('er' . DIRECTORY_SEPARATOR . 'iwtl_diagram.png')?>" alt="ER Diagram" class="img-fluid mx-auto d-block" style="max-width: 100%;">
+         </div>
+         <div class="p-2 mx-auto my-5 bg-light">
+            <h1 class="h1 text-dark px-5 mt-4">SQL code</h1>
+            <?php
+               $file = fopen(APP_ROOT . '/content/er/i_want_to_learn.sql', 'r');
+               // read and echo file line by line
+               echo '<pre class="my-5 mx-5">';
+               $counter = 0;
+               while(!feof($file)) 
+               {
+                  $line = fgets($file);
+                  echo '<code>';
+                  echo sprintf('%3d| ', $counter++);
+                  echo $line;
+                  echo '</code>';
+                  echo '<br/>';
+               }
+               echo '</pre>';
+            ?>
+         </div>
       </div>
-   </div>
+   </main>
 
 
 
