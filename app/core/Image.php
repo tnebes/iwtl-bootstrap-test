17a09087b4166202b072ae51debfc941c711ac72
tnebes <tomislav.nebes@gmail.com>
2021-08-05 11:25:59 +0200

    Updated diagram

--- app/core/Image.php
+++ app/core/Image.php
@@ -10,7 +10,14 @@
       public function getImage(string $imagePath, string $imageType = 'png') : void
       {
          $imagePath = APP_ROOT . DIRECTORY_SEPARATOR . 'content' . DIRECTORY_SEPARATOR . $imagePath;
-
-         echo 'data:image/' . $imageType . ';base64,' . base64_encode(file_get_contents($imagePath));
+         if (file_exists($imagePath))
+         {
+            echo 'data:image/' . $imageType . ';base64,' . base64_encode(file_get_contents($imagePath));
+         }
+         else
+         {
+            echo 'Image not found!';
+         }
+         
       }
    }
