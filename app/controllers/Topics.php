82ae9eaba94940a00e69279a40691f77e5bd474b
tnebes <tomislav.nebes@gmail.com>
2021-08-25 20:15:39 +0200

    Added skeleton for topics/questions

--- app/controllers/Topics.php
+++ app/controllers/Topics.php
@@ -4,11 +4,31 @@
    {
       public function __construct()
       {
-         
+         $this->model = $this->getModel('Topic');
       }
 
       public function index() : void
       {
          $this->view('topics/index');
+      }
+
+      public function topic() : void
+      {
+         $this->view('topics/topic');
+      }
+
+      public function create() : void
+      {
+         $this->view('topics/create');
+      }
+
+      public function edit() : void
+      {
+         $this->view('topics/edit');
+      }
+
+      public function delete() : void
+      {
+         $this->view('topics/delete');
       }
    }
---
+++ app/models/Topic.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+
+   Class Topic extends Model
+   {
+      public function __construct()
+      {
+         parent::__construct();
+         $this->TABLE_NAME = 'topic';
+      }
+
+      public function createTopic(stdClass $topic) : bool
+      {
+         return false;
+      }
+
+      public function getTopicsByUser(int $userId) : array
+      {
+         return [];
+      }
+
+      public function getTopicById(int $id) : ?stdClass
+      {
+         return null;
+      }
+
+      public function getTopicsBySubscription(int $subscriptionId) : array
+      {
+         return [];
+      }
+
+      public function editTopicById(stdClass $topic) : bool
+      {
+         
+         return false;
+      }
+
+      public function deleteTopicById(int $id) : bool
+      {
+         return false;
+      }
+
+   }
